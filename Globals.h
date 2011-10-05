83e43a22840d38e393025b5de3528af6d5ff45c2
Ryan Stevenson <rstevenson23@gmail.com>
2011-10-04 20:38:20 -0400

    Globals header file

---
+++ Globals.h
@@ -0,0 +1,42 @@
+#ifndef _GLOBALS_H
+#define _GLOBALS_H
+
+/* Types */
+enum y_pacingState {PERMANENT, TEMPORARY, PACE_NOW, MAGNET,
+                    POWER_ON_RESET} p_pacingState;
+                    
+enum y_pacingMode  {Off,AAT,VVT,AOO,AAI,VOO,VVI,VDD,DOO,DDI,
+                    DDD,AOOR,AAIR,VOOR,VVIR,DOOR,DDIR,DDDR} p_pacingMode;
+
+enum y_magnet  {INPLACE,NOT_INPLACE} m_magnet;    
+
+typedef enum
+{
+    FALSE=0,
+    TRUE
+} Bool; 
+
+/* Programmable Parameters */
+Bool p_hysteresis;
+unsigned int p_hysteresisInterval;
+unsigned int p_lowrateInterval;
+unsigned int p_vPaceAmp;
+unsigned float p_vPaceWidth;
+unsigned int p_VRP;
+unsigned int FNCODE;
+
+
+/* Baud rate for serial communication */
+#define BAUD_RATE 19200
+
+/* Frequency of the oscillator */
+#define FOSC 4000000
+
+#define k_egram 0x47
+#define k_echo  0x49
+#define k_estop 0x62
+#define k_pparams 0x55
+#define k_soh    0x01
+#define k_sync   0x16
+
+#endif
